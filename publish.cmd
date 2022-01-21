98a96f03ebf417f5e00f156bdb3a9495aae3aae4
Anujaa6Narvekar <45301485+Anujaa6Narvekar@users.noreply.github.com>
2022-01-21 09:53:15 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\anujadn-bot-9322-Web-Deploy.pubxml -p:Password=oesps7vvkPSxHytuxZKQvG08rlzeKqjL79eySpLDhTNk9MvvhW3DWtvMRnZ1
+
